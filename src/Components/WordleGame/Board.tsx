e3cc6c93d05c142fb3d8c9c1e7b2da0b8824c5bb
threegigs <yanghuaxuan@gmail.com>
2022-05-22 01:02:35 -0700

    Fix Wordle board for small phones

--- src/Components/WordleGame/Board.tsx
+++ src/Components/WordleGame/Board.tsx
@@ -24,6 +24,7 @@ export default function Board(props: {rowC: Readonly<number>}) {
 
     useEffect(() => {
         let currRow = 0;
+
         document.addEventListener("keydown", event => {
             if(CHARS.indexOf(event.key) > 0) {
             }
