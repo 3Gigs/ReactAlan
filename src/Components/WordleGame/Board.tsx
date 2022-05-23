8e244b4f8c10d744b9c7912ad7a1e686dda4fd6d
threegigs <yanghuaxuan@gmail.com>
2022-05-22 17:42:47 -0700

    Use uuid for React array lists, use grid

--- src/Components/WordleGame/Board.tsx
+++ src/Components/WordleGame/Board.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Row from "./Row";
 
 const CHARS = "abcdefghijklmnopqrstuvwxyz";
@@ -10,6 +11,7 @@ type BoardProps = {
 
 export default function Board({ rowC }: BoardProps) {
     const [rows, setRows] = useState<JSX.Element[]>([]);
+    const [currRow, setCurrRow] = useState(0);
     const [currInput, setInput] = useState("");
 
     /**
@@ -19,19 +21,29 @@ export default function Board({ rowC }: BoardProps) {
         const result: JSX.Element[] = [];
 
         for (let i = 0; i < rowC; i++) {
-            result.push(<Row cols={WLENGTH} />);
+            result.push(<Row cols={WLENGTH} key={uuidv4()} />);
         }
         setRows(result);
     }, [rowC]);
-
     /**
      * Handle keyboard and input
      */
     useEffect(() => {
+        function setRow(index: number, word: string) {
+            const newRows = rows.map((e: JSX.Element, i: number) => {
+                if (i === index) {
+                    return <Row cols={WLENGTH} word={word} key={uuidv4()} />;
+                }
+                return e;
+            });
+            setRows(newRows);
+        }
+
         const appendCharToInput = (n: string) => setInput((e) => e + n);
         const handler = (e: KeyboardEvent) => {
             if (!e.repeat && CHARS.indexOf(e.key) >= 0) {
                 appendCharToInput(e.key);
+                setRow(currRow, currInput);
                 console.log(currInput);
             }
         };
@@ -40,7 +52,7 @@ export default function Board({ rowC }: BoardProps) {
         return function cleanup() {
             document.removeEventListener("keydown", handler);
         };
-    }, [currInput]);
+    }, [currInput, currRow, rows]);
 
     return <div className="Board">{rows}</div>;
 }
--- src/Components/WordleGame/Row.tsx
+++ src/Components/WordleGame/Row.tsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "uuid";
 import Square from "./Square";
 
 type RowProps = {
@@ -5,12 +6,20 @@ type RowProps = {
     word?: string
 }
 
+function getCharFromWord(word: string, index: number) {
+    const result = word.charAt(index);
+    if (result.length <= 0) {
+        return undefined;
+    }
+    return result;
+}
+
 export default function Row({ cols, word }: RowProps) {
     function rows() {
         let result: JSX.Element[] = [];
 
         for (let i = 0; i < cols; i++) {
-            result = [...result, <Square char={(word ? word.charAt(i) : undefined) ?? "⠀"} status={1} />];
+            result = [...result, <Square char={(word ? getCharFromWord(word, i) : undefined) ?? "⠀"} status={1} key={uuidv4()} />];
         }
 
         return result;
