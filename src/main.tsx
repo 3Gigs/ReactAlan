1b6d0c44cb93062caf3f3237ba2b55644491ba42
threegigs <yanghuaxuan@gmail.com>
2022-05-28 01:37:34 -0700

    Route to wordle

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App";
 import Wordle from "./Routes/Wordle";
 import "./css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/Wordle" element={<Wordle />} />
+            <Route path="/" element={<Wordle />} />
         </Routes>
     </BrowserRouter>,
 );
