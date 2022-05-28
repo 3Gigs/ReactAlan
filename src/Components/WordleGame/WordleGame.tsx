bae5f7041b0a233d76b93a0bf2c1989440e23545
threegigs <yanghuaxuan@gmail.com>
2022-05-27 22:50:55 -0700

    Use random words api

--- src/Components/WordleGame/WordleGame.tsx
+++ src/Components/WordleGame/WordleGame.tsx
@@ -1,19 +1,26 @@
-import { useEffect } from "react";
-import { gameOptions } from "../../../config.json";
+import { useEffect, useState } from "react";
+import { gameOptions, dictionaryAPI } from "../../../config.json";
 import Board from "./Board";
 import "../../css/Wordle.css";
 import StatModal from "./StatModal";
 import initAlanBtn from "../../utils/initAlanBtn";
 
 export default function WordleGame() {
+    const [word, setWord] = useState("");
+
     useEffect(() => {
         initAlanBtn();
     }, []);
 
+    useEffect(() => {
+        const fetchWord = async () => fetch(dictionaryAPI);
+        fetchWord().then((r) => r.text().then((w) => setWord(w)));
+    }, [word]);
+
     return (
         <div>
             <StatModal />
-            <Board rowC={gameOptions.TRIES} word="cools" />
+            <Board rowC={gameOptions.TRIES} word={word} />
         </div>
     );
 }
